039f6fdc700b200d34a716113ed36c60bb3e20bb
PINGILI SAKETH REDDY <sakethreddy7660@gmail.com>
2026-09-06 16:14:07 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,6 +2,5 @@ import os
 
 class Config:
     DATABASE_URL = os.getenv(
-        "DATABASE_URL",
-        "postgresql://postgres:bunny766@localhost:5432/crop_project"
+        "DATABASE_URL"
     )
